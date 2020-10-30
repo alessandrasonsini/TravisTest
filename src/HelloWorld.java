c6a689319e29b4d24be86db9ef01e596fbf8d9c8
alessandrasonsini <56720833+alessandrasonsini@users.noreply.github.com>
2020-10-30 12:05:46 +0100

    Test modify java file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello wolrd!");
+		System.out.println("Hello wolrd!\n");
+		System.out.println("test");
 	}
 }
